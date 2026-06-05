6380df80d38a3bb37c6b0bbb8b8f8b30f2faa82c
Matt Evans <matt@ozlabs.org>
2026-06-05 17:11:12 +0100

    vfio/pci: Convert BAR mmap() to use a DMABUF
    Convert the VFIO device fd fops->mmap to create a DMABUF representing
    the BAR mapping, and make the VMA fault handler look up PFNs from the
    corresponding DMABUF.  This supports future code mmap()ing BAR
    DMABUFs, and iommufd work to support Type1 P2P.
    
    First, vfio_pci_core_mmap() uses the new
    vfio_pci_core_mmap_prep_dmabuf() helper to export a DMABUF
    representing a single BAR range.  Then, the vfio_pci_mmap_huge_fault()
    callback is updated to understand revoked buffers, and uses the new
    vfio_pci_dma_buf_find_pfn() helper to determine the PFN for a given
    fault address.
    
    Now that the VFIO DMABUFs can be mmap()ed, vfio_pci_dma_buf_move()
    zaps PTEs (used on the revocation and cleanup paths).
    
    CONFIG_VFIO_PCI_CORE now unconditionally selects
    CONFIG_DMA_SHARED_BUFFER.  The CONFIG_VFIO_PCI_DMABUF feature
    conditionally includes support for VFIO_DEVICE_FEATURE_DMA_BUF,
    depending on the availability of CONFIG_PCI_P2PDMA.
    
    When the DMABUF feature is not present, DMABUFs are used internally
    for the BAR mappings.  In this case, DMABUF attach is prohibited (and
    the requirement for a P2PDMA provider is relaxed).
    
    Signed-off-by: Matt Evans <matt@ozlabs.org>

--- drivers/vfio/pci/vfio_pci_dmabuf.c
+++ drivers/vfio/pci/vfio_pci_dmabuf.c
@@ -9,6 +9,7 @@
 
 MODULE_IMPORT_NS("DMA_BUF");
 
+#ifdef CONFIG_VFIO_PCI_DMABUF
 static int vfio_pci_dma_buf_attach(struct dma_buf *dmabuf,
 				   struct dma_buf_attachment *attachment)
 {
@@ -25,6 +26,18 @@ static int vfio_pci_dma_buf_attach(struct dma_buf *dmabuf,
 
 	return 0;
 }
+#else
+static int vfio_pci_dma_buf_attach(struct dma_buf *dmabuf,
+				   struct dma_buf_attachment *attachment)
+{
+	/*
+	 * Explicit export can't occur without the DMABUF feature, but
+	 * DMABUFs are implicitly created for BAR mappings.  An
+	 * .attach that fails prevents dma_buf_attach().
+	 */
+	return -EOPNOTSUPP;
+}
+#endif /* CONFIG_VFIO_PCI_DMABUF */
 
 static void vfio_pci_dma_buf_done(struct kref *kref)
 {
@@ -296,6 +309,7 @@ static int vfio_pci_dmabuf_export(struct vfio_pci_core_device *vdev,
 	return 0;
 }
 
+#ifdef CONFIG_VFIO_PCI_DMABUF
 /*
  * This is a temporary "private interconnect" between VFIO DMABUF and iommufd.
  * It allows the two co-operating drivers to exchange the physical address of
@@ -494,6 +508,7 @@ err_free_ranges:
 	kfree(dma_ranges);
 	return ret;
 }
+#endif /* CONFIG_VFIO_PCI_DMABUF */
 
 int vfio_pci_core_mmap_prep_dmabuf(struct vfio_pci_core_device *vdev,
 				   struct vm_area_struct *vma,
@@ -532,8 +547,13 @@ int vfio_pci_core_mmap_prep_dmabuf(struct vfio_pci_core_device *vdev,
 	priv->nr_ranges = 1;
 	priv->vma_pgoff_adjust = vma->vm_pgoff;
 
+	/*
+	 * The provider can be NULL _iff_ the DMABUF feature isn't
+	 * supported, because it's only used by DMABUF import and
+	 * attach is prohibited if the feature isn't present.
+	 */
 	priv->provider = pcim_p2pdma_provider(vdev->pdev, res_index);
-	if (!priv->provider) {
+	if (IS_ENABLED(CONFIG_VFIO_PCI_DMABUF) && !priv->provider) {
 		ret = -EINVAL;
 		goto err_free_phys;
 	}
@@ -592,6 +612,8 @@ void vfio_pci_dma_buf_move(struct vfio_pci_core_device *vdev, bool revoked)
 			if (revoked) {
 				kref_put(&priv->kref, vfio_pci_dma_buf_done);
 				wait_for_completion(&priv->comp);
+				unmap_mapping_range(priv->dmabuf->file->f_mapping,
+						    0, 0, true);
 				/*
 				 * Re-arm the registered kref reference and the
 				 * completion so the post-revoke state matches the
